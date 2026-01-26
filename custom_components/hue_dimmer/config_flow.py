d8bedb37aca1e7e205fa51b8459566e9567998b2
jasonmx <jason@merron.net>
2026-01-26 13:00:23 +0000

    Fixed UI card not appearing (integration type can't be Helper type)

--- custom_components/hue_dimmer/config_flow.py
+++ custom_components/hue_dimmer/config_flow.py
@@ -1,8 +1,10 @@
+import voluptuous as vol
 from homeassistant import config_entries
 from .const import DOMAIN
 
 class HueDimmerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Philips Hue Smooth Dimmer."""
+
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
@@ -12,3 +14,8 @@ class HueDimmerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         if user_input is not None:
             return self.async_create_entry(title="Philips Hue Smooth Dimmer", data={})
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({}),
+        )
